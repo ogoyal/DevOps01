251c1ee5ca73bba9cba1feb49e2898be291bcc1e
Ojas Goyal <ojasgoyal@localhost.localdomain>
2018-04-14 15:49:22 -0700

    ip address & hardware address

--- devops01a.sh
+++ devops01a.sh
@@ -12,15 +12,19 @@ echo "Free space on each volume: "
 df | awk '{printf "%-25s %5s\n",$1, $4}'
 
 printf "\n"
-echo "Number of cpu/cores"
+echo "Number of cpu/cores: "
 grep -c ^processor /proc/cpuinfo
 
 printf "\n"
-echo "Information about the cpu/cores"
+echo "Information about the cpu/cores: "
 lscpu
 
 printf "\n"
-echo "Amount of ram"
+echo "Amount of ram: "
 free -m
 
-echo "ip-address: `/sbin/ifconfig | head -2 | grep 'inet ' | tr -s ' ' | sed 's/addr://g' |cut -d' ' -f3` mac-address: `/sbin/ifconfig | head -1 | tr -s ' ' | cut -d' ' -f5 | tr ':' ' ' |tr 'a-z' 'A-Z'|sed 's/ //g'`"
+printf "\n"
+echo "IP Address: `ifconfig | head -2 | grep 'inet ' | tr -s ' ' | sed 's/addr://g' |cut -d' ' -f3`"
+echo "MAC Address: `ifconfig | head -1 | tr -s ' ' | cut -d' ' -f5 | tr ':' ' ' |tr 'a-z' 'A-Z'|sed 's/ //g'`"
+
+

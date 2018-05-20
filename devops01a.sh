c56332696563b06c6419ba1f5e4fb714e085b88e
Ojas Goyal <ojasgoyal@localhost.localdomain>
2018-05-20 10:17:40 -0700

    Part 2 & 3

--- devops01a.sh
+++ devops01a.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 volume() {
+  echo "***VOLUME***"
   vol_num=`expr $(df | wc -l) - 1`
   echo "Number of volumes: $vol_num"
 
@@ -11,28 +12,36 @@ volume() {
   printf "\n"
   echo "Free space on each volume: "
   df | awk '{printf "%-25s %5s\n",$1, $4}'
+  
+  printf "\n"
 }
 
 cpu() {
-  printf "\n"
-  echo "Number of cpu/cores: "
-  grep -c ^processor /proc/cpuinfo
+  echo "***CPU***"
+  cpu_num=`expr $(grep -c ^processor /proc/cpuinfo)`
+  echo "Number of cpu/cores: $cpu_num"
 
   printf "\n"
   echo "Information about the cpu/cores: "
   lscpu
+
+  printf "\n"
 }
 
 ram() {
-  printf "\n"
-  echo "Amount of ram: "
+  echo "***RAM***"
   free -m
+
+  printf "\n"
 }
 
 network() {
+  echo "***NETWORK***"
+  echo "IP Address: `hostname -I`"
+  IFACE=`ip route | awk '{print $3}'`
+  echo "MAC Address: `cat /sys/class/net/$IFACE/address`"
+
   printf "\n"
-  echo "IP Address: `ifconfig | head -2 | grep 'inet ' | tr -s ' ' | sed 's/addr://g' |cut -d' ' -f3`"
-  echo "MAC Address: `ifconfig | head -1 | tr -s ' ' | cut -d' ' -f5 | tr ':' ' ' |tr 'a-z' 'A-Z'|sed 's/ //g'`"
 }
 
 volume
--- devops01b.sh
+++ devops01b.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
+# Import functions from devops01a.sh
 source "$(pwd)/devops01a.sh" >/dev/null
 
-if [ "$1" == "-all" ]; then
-    volume
-    cpu
-    ram
-    network
-elif [ "$1" == "-volumes" ]; then
-    volume
-elif [ "$1" == "-cpu" ]; then
-    cpu
-elif [ "$1" == "-ram" ]; then
-    ram
-elif [ "$1" == "-network" ]; then
-    network
+if [ -z "$1" ]; then
+    echo "[-volumes] : display volume information."
+    echo "[-cpu]     : display cpu information."
+    echo "[-ram]     : display ram information."
+    echo "[-network] : display mac & ip address."
+    echo "[-all]     : display information for all command line options."
 else
-    echo "-volumes : display volume information"
-    echo "-cpu : display cpu information"
-    echo "-ram : display ram information"
-    echo "-network : display mac & ip address"
-    echo "-all : display information for all command line options"
-fi
+    for i in "$@" ; do
+        if [[ $i == "-all" ]]; then
+            volume; cpu; ram; network;
+            exit 1;
+        fi
+    done
 
+    for i in "$@" ; do
+        if [[ $i == "-volumes" ]]; then
+            volume;
+        fi
+        if [[ $i == "-cpu" ]]; then
+            cpu;
+        fi
+        if [[ $i == "-ram" ]]; then
+            ram;
+        fi
+        if [[ $i == "-network" ]]; then
+            network;
+        fi
+    done
+
+fi
 
